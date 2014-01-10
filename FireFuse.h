7325ca041266384688621ec8bc91e76ea511fa82
Karl Lew <karl@firepick.org>
2014-01-09 21:13:35 -0800

    FireFUse.h

---
+++ FireFuse.h
@@ -0,0 +1,44 @@
+/*
+FireFuse.h https://github.com/firepick1/FireFuse/wiki
+
+Copyright (C) 2013,2014  Karl Lew, <karl@firepick.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef FIREFUSE_H
+#define FIREFUSE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_GCODE_LEN 255 /* maximum characters in a gcode instruction */
+
+#define STATUS_PATH "/status"
+#define HOLES_PATH "/holes"
+#define FIRESTEP_PATH "/firestep"
+#define FIRELOG_PATH "/firelog"
+#define CAM_PATH "/cam.jpg"
+
+typedef struct {
+  char *pData;
+  int length;
+  long reserved;
+} JPG;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
