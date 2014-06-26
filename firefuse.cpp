44bf819ca5ca54b3ea20da064262fc7f6d1c7140
firepick1 (pixel) <karl@firepick.org>
2014-06-26 06:23:36 -0700

    ...

---
+++ firefuse.cpp
@@ -0,0 +1,21 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include "version.h"
+#include "FirePiCam.h"
+#include "FireLog.h"
+#include "firefuse.h"
+
+int firefuse_main(int argc, char *argv[]) {
+  return firefuse_main(argc, argv);
+}
+
+
