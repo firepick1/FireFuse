9db099f082b3e7716d8ed6e70bf696f7ab29d73c
firepick1 (firepick80) <karl@firepick.org>
2014-06-28 20:29:58 -0700

    compiler

--- firefuse.cpp
+++ firefuse.cpp
@@ -1,18 +1,16 @@
-#include <dirent.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "FireSight.hpp"
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
 #include <time.h>
-#include <pthread.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "firefuse.h"
 #include "version.h"
 #include "FirePiCam.h"
-#include "FireLog.h"
-#include "firefuse.h"
 
 int firefuse_main(int argc, char *argv[]) {
   return firefuse_main(argc, argv);
