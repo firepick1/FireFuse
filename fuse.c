644d776baf625bf0cd52d41c2b1fd2e3922f3edb
firepick1 (pixel) <karl@firepick.org>
2015-02-27 12:27:13 -0800

    offset

--- fuse.c
+++ fuse.c
@@ -324,14 +324,17 @@ int firefuse_write(const char *path, const char *buf, size_t bufsize, off_t offs
     if (is_cv_path(path)) {
         return cve_write(path, buf, bufsize, offset, fi);
     }
-    if (offset) {
 		char temp[256];
 		size_t bytes = (bufsize >= 255) ? 255 : bufsize;
 		memcpy(temp, buf, bytes);
 		temp[bytes] = 0;
-        LOGERROR3("firefuse_write %s -> %s non-zero offset:%ld", path, temp, (long) offset);
+    if (offset) {
+        LOGERROR3("firefuse_write %s -> %s offset:%ld", path, temp, (long) offset);
         return EINVAL;
+	} else {
+        LOGINFO3("firefuse_write %s -> %s offset:%ld", path, temp, (long) offset);
     }
+
     if (is_cnc_path(path)) {
         return cnc_write(path, buf, bufsize, offset, fi);
     }
